0a8ab0ab7e707bc2c4ce2cea3fdc5734aec4c6fd
joshuajallen <103208650+joshuajallen@users.noreply.github.com>
2022-12-06 11:47:02 +0000

    Create cross_validation.py

---
+++ functions/cross_validation.py
@@ -0,0 +1,63 @@
+class RandomSearch(object):
+    
+    def __init__(self,X_train,y_train,model,hyperparameters):
+        
+        self.X_train = X_train
+        self.y_train = y_train
+        self.model = model
+        self.hyperparameters = hyperparameters
+        
+    def RandomSearch(self):
+        # Create randomized search 10-fold cross validation and 100 iterations
+        cv = 10
+        clf = RandomizedSearchCV(self.model,
+                                 self.hyperparameters,
+                                 random_state=1,
+                                 n_iter=100,
+                                 cv=cv,
+                                 verbose=0,
+                                 n_jobs=-1,
+                                 )
+        # Fit randomized search
+        best_model = clf.fit(self.X_train, self.y_train)
+        message = (best_model.best_score_, best_model.best_params_)
+        print("Best: %f using %s" % (message))
+
+        return best_model,best_model.best_params_
+    
+    def BestModelPridict(self,X_test):
+        
+        best_model,_ = self.RandomSearch()
+        pred = best_model.predict(X_test)
+        return pred
+        
+  class GridSearch(object):
+    
+    def __init__(self,X_train,y_train,model,hyperparameters):
+        
+        self.X_train = X_train
+        self.y_train = y_train
+        self.model = model
+        self.hyperparameters = hyperparameters
+        
+    def GridSearch(self):
+        # Create randomized search 10-fold cross validation and 100 iterations
+        cv = 10
+        clf = GridSearchCV(self.model,
+                                 self.hyperparameters,
+                                 cv=cv,
+                                 verbose=0,
+                                 n_jobs=-1,
+                                 )
+        # Fit randomized search
+        best_model = clf.fit(self.X_train, self.y_train)
+        message = (best_model.best_score_, best_model.best_params_)
+        print("Best: %f using %s" % (message))
+
+        return best_model,best_model.best_params_
+    
+    def BestModelPridict(self,X_test):
+        
+        best_model,_ = self.GridSearch()
+        pred = best_model.predict(X_test)
+        return pred
