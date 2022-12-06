206c7be05a0f063631489a144996b5dd93803d1a
joshuajallen <103208650+joshuajallen@users.noreply.github.com>
2022-12-06 13:03:21 +0000

    Update plots.py

--- plots/plots.py
+++ plots/plots.py
@@ -89,3 +89,179 @@ def OutLierDetection(df,feature1,feature2,outliers_fraction=.1):
 
     plt.show()
     return new_df
+
+
+
+
+def plotBarCat(df,feature,target):
+    
+    
+    
+    x0 = df[df[target]==0][feature]
+    x1 = df[df[target]==1][feature]
+
+    trace1 = go.Histogram(
+        x=x0,
+        opacity=0.75
+    )
+    trace2 = go.Histogram(
+        x=x1,
+        opacity=0.75
+    )
+
+    data = [trace1, trace2]
+    layout = go.Layout(barmode='overlay',
+                      title=feature,
+                       yaxis=dict(title='Count'
+        ))
+    fig = go.Figure(data=data, layout=layout)
+
+    py.iplot(fig, filename='overlaid histogram')
+    
+    def DescribeFloatSkewKurt(df,target):
+        """
+            A fundamental task in many statistical analyses is to characterize
+            the location and variability of a data set. A further
+            characterization of the data includes skewness and kurtosis.
+            Skewness is a measure of symmetry, or more precisely, the lack
+            of symmetry. A distribution, or data set, is symmetric if it
+            looks the same to the left and right of the center point.
+            Kurtosis is a measure of whether the data are heavy-tailed
+            or light-tailed relative to a normal distribution. That is,
+            data sets with high kurtosis tend to have heavy tails, or
+            outliers. Data sets with low kurtosis tend to have light
+            tails, or lack of outliers. A uniform distribution would
+            be the extreme case
+        """
+        print('-*-'*25)
+        print("{0} mean : ".format(target), np.mean(df[target]))
+        print("{0} var  : ".format(target), np.var(df[target]))
+        print("{0} skew : ".format(target), skew(df[target]))
+        print("{0} kurt : ".format(target), kurtosis(df[target]))
+        print('-*-'*25)
+    
+    DescribeFloatSkewKurt(df,target)
+    
+    
+def plotHist(df,nameOfFeature):
+    cls_train = df[nameOfFeature]
+    data_array = cls_train
+    hist_data = np.histogram(data_array)
+    binsize = .5
+
+    trace1 = go.Histogram(
+        x=data_array,
+        histnorm='count',
+        name='Histogram of Wind Speed',
+        autobinx=False,
+        xbins=dict(
+            start=df[nameOfFeature].min()-1,
+            end=df[nameOfFeature].max()+1,
+            size=binsize
+        )
+    )
+
+    trace_data = [trace1]
+    layout = go.Layout(
+        bargroupgap=0.3,
+         title='The distribution of ' + nameOfFeature,
+        xaxis=dict(
+            title=nameOfFeature,
+            titlefont=dict(
+                family='Courier New, monospace',
+                size=18,
+                color='#7f7f7f'
+            )
+        ),
+        yaxis=dict(
+            title='Number of labels',
+            titlefont=dict(
+                family='Courier New, monospace',
+                size=18,
+                color='#7f7f7f'
+            )
+        )
+    )
+    fig = go.Figure(data=trace_data, layout=layout)
+    py.iplot(fig)
+    
+def OutLiersBox(df,nameOfFeature):
+    
+    trace0 = go.Box(
+        y = df[nameOfFeature],
+        name = "All Points",
+        jitter = 0.3,
+        pointpos = -1.8,
+        boxpoints = 'all',
+        marker = dict(
+            color = 'rgb(7,40,89)'),
+        line = dict(
+            color = 'rgb(7,40,89)')
+    )
+
+    trace1 = go.Box(
+        y = df[nameOfFeature],
+        name = "Only Whiskers",
+        boxpoints = False,
+        marker = dict(
+            color = 'rgb(9,56,125)'),
+        line = dict(
+            color = 'rgb(9,56,125)')
+    )
+
+    trace2 = go.Box(
+        y = df[nameOfFeature],
+        name = "Suspected Outliers",
+        boxpoints = 'suspectedoutliers',
+        marker = dict(
+            color = 'rgb(8,81,156)',
+            outliercolor = 'rgba(219, 64, 82, 0.6)',
+            line = dict(
+                outliercolor = 'rgba(219, 64, 82, 0.6)',
+                outlierwidth = 2)),
+        line = dict(
+            color = 'rgb(8,81,156)')
+    )
+
+    trace3 = go.Box(
+        y = df[nameOfFeature],
+        name = "Whiskers and Outliers",
+        boxpoints = 'outliers',
+        marker = dict(
+            color = 'rgb(107,174,214)'),
+        line = dict(
+            color = 'rgb(107,174,214)')
+    )
+
+    data = [trace0,trace1,trace2,trace3]
+
+    layout = go.Layout(
+        title = "{} Outliers".format(nameOfFeature)
+    )
+
+    fig = go.Figure(data=data,layout=layout)
+    py.iplot(fig, filename = "Outliers")
+    
+class PlotBoxR(object):
+    
+    
+    def __Trace(self,nameOfFeature,value): 
+    
+        trace = go.Box(
+            y=value,
+            name = nameOfFeature,
+            marker = dict(
+                color = 'rgb(0, 128, 128)',
+            )
+        )
+        return trace
+
+    def PlotResult(self,names,results):
+        
+        data = []
+
+        for i in range(len(names)):
+            data.append(self.__Trace(names[i],results[i]))
+
+
+        py.iplot(data)
